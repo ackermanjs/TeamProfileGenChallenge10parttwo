f51e417fbaf8556fc71632ce6f0e5c87aea0b38f
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-03-19 16:43:54 -0600

    working on cards for the three positions to be displayed

---
+++ src/cards.js
@@ -0,0 +1,30 @@
+const createManagerCard = (manager) => {
+    return `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${manager.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">EMPLOYEE-ID: ${manager.id}</h6>
+      <h6 class="card-subtitle mb-2 text-muted">E-MAIL ADDRESS: ${manager.mail}</h6>
+      <h6 class="card-subtitle mb-2 text-muted">MANAGER OFFICE NUMBER: ${manager.contact}</h6>
+    </div>
+  </div>`;
+};
+const createEngineerCard = (engineer) => {
+    return `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${engineer.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">EMPLOYEE ID: ${engineer.id}</h6>
+      <h6 class="card-subtitle mb-2 text-muted">E-MAIL ADDRESS: ${engineer.mail}</h6>
+      <h6 class="card-subtitle mb-2 text-muted">GITHUB URL: ${engineer.hub}</h6>
+    </div>
+  </div>`;
+};
+const createInternCard = (intern) => {
+    return `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${intern.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">EMPLOYEE ID: ${intern.id}</h6>
+      <h6 class="card-subtitle mb-2 text-muted">E-MAIL ADDRESS: ${intern.mail}</h6>
+      <h6 class="card-subtitle mb-2 text-muted">SCHOOL: ${intern.edu}</h6>
+    </div>
+  </div>`;
+};
