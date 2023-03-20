51c1b17742bb5db1443606d1e8eeac42ba57da45
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-03-19 19:36:42 -0600

    finishing up, just about all functionality it there. fs is generating html doc, but for some reason everything is displaying as an intern even though prompts are working as intended

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-// const populateDoc = require("add my file path when i get it sorted");
+const populateProfiles = require("./src/doctemplate");
 
-// const Engineer = require("./lib/Engineer");
+const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
-// const Intern = require("./lib/Intern");
+const Intern = require("./lib/Intern");
 
 let roster = [];
 
@@ -46,13 +46,13 @@ const createNewEmployee = () => {
       {
         type: "list",
         name: "position",
-        message: "What this new Employee's Position?",
-        choices: ["Enigneer, Intern"],
+        message: "What is the Employee's Position?",
+        choices: ["Engineer", "Intern"],
       },
       {
         type: "input",
         name: "name",
-        message: "What is this new Employee's Name?",
+        message: "What is the Employee's Name?",
       },
       {
         type: "input",
@@ -62,7 +62,7 @@ const createNewEmployee = () => {
       {
         type: "input",
         name: "mail",
-        message: "What is this Employee's E-mail Address?",
+        message: "What is the Employee's E-mail Address?",
       },
       {
         type: "input",
@@ -93,11 +93,24 @@ const createNewEmployee = () => {
       if (position === "Engineer") {
         const engineer = new Engineer(name, id, mail, hub);
         roster.push(engineer);
-
       } else if (position === "Intern") {
         const intern = new Intern(name, id, mail, edu);
         roster.push(intern);
       }
+
+      if (nextStep) {
+        return createNewEmployee(roster);
+      } else {
+        return roster;
+      }
     });
 };
-createNewManager();
+createNewManager()
+  .then(createNewEmployee)
+  .then((roster) => {
+    const cardDisplay = populateProfiles(roster);
+    fs.writeFile("./dist/index.html", cardDisplay, (err) => {
+      if (err) throw new Error(err);
+      console.log("Your Team has been created!");
+    });
+  });
--- src/cards.js
+++ src/cards.js
@@ -1,7 +1,8 @@
 const createManagerCard = (manager) => {
-    return `<div class="card" style="width: 18rem;">
+  return `<div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">${manager.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">Position: Manager</h6>
       <h6 class="card-subtitle mb-2 text-muted">EMPLOYEE-ID: ${manager.id}</h6>
       <h6 class="card-subtitle mb-2 text-muted">E-MAIL ADDRESS: ${manager.mail}</h6>
       <h6 class="card-subtitle mb-2 text-muted">MANAGER OFFICE NUMBER: ${manager.contact}</h6>
@@ -9,9 +10,10 @@ const createManagerCard = (manager) => {
   </div>`;
 };
 const createEngineerCard = (engineer) => {
-    return `<div class="card" style="width: 18rem;">
+  return `<div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">${engineer.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">Position: Engineer</h6>
       <h6 class="card-subtitle mb-2 text-muted">EMPLOYEE ID: ${engineer.id}</h6>
       <h6 class="card-subtitle mb-2 text-muted">E-MAIL ADDRESS: ${engineer.mail}</h6>
       <h6 class="card-subtitle mb-2 text-muted">GITHUB URL: ${engineer.hub}</h6>
@@ -19,12 +21,17 @@ const createEngineerCard = (engineer) => {
   </div>`;
 };
 const createInternCard = (intern) => {
-    return `<div class="card" style="width: 18rem;">
+  return `<div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">${intern.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">Position: Intern</h6>
       <h6 class="card-subtitle mb-2 text-muted">EMPLOYEE ID: ${intern.id}</h6>
       <h6 class="card-subtitle mb-2 text-muted">E-MAIL ADDRESS: ${intern.mail}</h6>
       <h6 class="card-subtitle mb-2 text-muted">SCHOOL: ${intern.edu}</h6>
     </div>
   </div>`;
 };
+
+module.exports = createManagerCard;
+module.exports = createEngineerCard;
+module.exports = createInternCard;
